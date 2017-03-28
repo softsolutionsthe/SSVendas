b3e0cc85b792e7b361debe831e03fd2358fe5616
softsolutionsthe <marcielsilva_santos@hotmail.com>
2017-03-28 18:46:39 -0300

    Update update.bat

--- update.bat
+++ update.bat
@@ -10,10 +10,6 @@ xcopy conexao.udl %tmp%"\SoftSolutions\bk\"
 xcopy configs.ini %tmp%"\SoftSolutions\bk\"
 xcopy "Certificados\certificado.pfx" %tmp%"\SoftSolutions\bk\"
 
-mkdir  %tmp%"\SoftSolutions\downloads\SSVendas"
-echo "Aguardando baixar os arquivos."
-TortoiseProc.exe /notempfile /command:update /path:%tmp%\SoftSolutions\downloads\SSVendas
-
 del /s /q ""
 rd /s /q "Certificados"
 rd /s /q "Eventos"
@@ -22,16 +18,11 @@ rd /s /q "Schemas"
 rd /s /q "XMLs"
 rd /s /q "MinGW"
 
-xcopy  /E /C %tmp%"\SoftSolutions\downloads\SSVendas\trunk"
-
-del /s /q "conexao.udl"
-del /s /q "certificado.pfx"
-del /s /q "configs.ini"
-
-xcopy %tmp%"\SoftSolutions\bk\conexao.udl"
-xcopy %tmp%"\SoftSolutions\bk\configs.ini"
-xcopy %tmp%"\SoftSolutions\bk\certificado.pfx" "Certificados"
+mkdir  %tmp%"\SoftSolutions\downloads\SSVendas"
+echo "Aguardando baixar os arquivos."
+TortoiseProc.exe /notempfile /command:update /path:%tmp%\SoftSolutions\downloads\SSVendas
 
 attrib -r -s -h update.bat
 
 start instala.bat
+del /s /q instala.bat
